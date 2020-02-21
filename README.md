cb55634fc15349fb0ccaa80319ebed49e39e1351
Geethanjali Veetil <gtveetil@gmail.com>
2020-02-21 12:45:10 +1100

    Updated Readme, added comments refactored code

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# JavaScript-Password-Generator
-Application that generates a random password based on user-selected criteria.
+# JavaScript Password Generator
+
+ [![Image of Password Generator Icon](assets/images/readme_password_generator_icon.jpg "Open the Password Generator.")](https://gveetil.github.io/JavaScript-Password-Generator/)
+
+## Description
+This application generates a random password based on user-selected criteria. Users can choose the password length and the type of characters to be used while generating the password. The newly generated password is then displayed on the screen. 
+
+## Features
+### Strong Password 
+This application generates a strong password based on user-selected criteria. Using a strong password provides users with greater security when they need to access sensitive data. This application uses javascript to generate the strong password, and dynamically update HTML elements.
+
+### Responsive Web Design 
+This application is built using Responsive web design principles. Responsive web design provides better accessibility and an optimal viewing experience to the user, by ensuring that web pages render well on different devices and window / screen sizes. This has been achieved with the use of CSS to style HTML elements.
+
+## Functionality
+
+![Image of Password Generator Page](assets/images/readme_password_generator_page.jpg "Password Generator Page")
+
+The Password Generator page contains a form that allows users to generate a strong password. When the user clicks the *Generate Password* button, the application prompts the user to enter the length of the desired password.
+
+#### Password Length Selection
+
+![Image of Password Length Selection](assets/images/readme_prompt_length.jpg "Password Length Selection")
+
+* The password length must be between 8 and 128 characters. 
+* A default password length of 8 is displayed initially, and the user can then edit this.
+* Once a password has been generated, the password length defaults to the user's last selection. 
+* If the user enters an invalid length, the user is prompted to correct the entry and an *Incorrect selection !!* message is displayed.
+* If the user chooses to cancel the operation, the prompt exits without generating a password. 
+
+Once the password length has been selected, the application then prompts the user to choose the character types to be included in the desired password.
+
+#### Password Character Types Selection
+
+![Image of Password Character Type Selection](assets/images/readme_prompt_char_types.jpg "Password Character Type Selection")
+
+* The user needs to enter the password character types from a selection of lowercase(l), uppercase(u), numeric(n), and special characters(s). 
+* By default all the password character types are selected initially, and the user can then edit this.
+* Once a password has been generated, the password character type defaults to the user's last selection. 
+* If the user enters an invalid selection or does not choose at least one type, the user is prompted to correct the entry and an *Incorrect selection !!* message is displayed.
+* If the user chooses to cancel the operation, the prompt exits without generating a password. 
+
+Once the password character types has been selected, the application then generates a password that matches the selected criteria and then displays it in the box above the *Generate Password* button, on the main page. 
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,12 @@
-// Assignment Code
+// Password Generator Form Elements referenced by this code
 var generateBtn = document.querySelector("#generate");
-// Text displayed while prompting user input
+var passwordTextEl = document.querySelector("#password");
+
+// Text displayed while prompting user for input
 const promptPasswordLengthText = "Please enter the length of your password.\nThe Password must be 8 - 128 characters long."
 const promptPasswordCharsText = "Please select the character types to include in your password. \n l - lowercase, u - uppercase, n - numeric, s - special characters"
 const promptIncorrectEntry = "Incorrect selection !!\n"
+
 // Character set to be used while generating the password - l - lowercase, u - uppercase, n - numeric, s - special characters
 var passwordCharset = {
   "l": "abcdefghijklmnopqrstuvwxyz",
@@ -16,36 +19,36 @@ var passwordCharset = {
 var pwdLengthSelected = 8;
 var pwdCharTypesSelected = "luns";
 
-// Write password to the #password input
+// Generate and display password in the password text area element
 function writePassword() {
   var password = generatePassword();
+  // If the password was generated successfully, display it 
   if (password != "") {
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
+    passwordTextEl.value = password;
   }
 }
 
 // Function to generate password based on user input
 // Returns - new password if successful, empty string if cancelled by user 
 function generatePassword() {
-  // Set the password length and chars and proceed with generation only if successful
-  if (setPasswordLength() && setPasswordChars()) {
-    //Generate and return password
-    var newPassword = "";
-    var selectedCharset = getPasswordCharset();
-    var charsetLength = selectedCharset.length;
-    for (let index = 0; index < pwdLengthSelected; index++) {
-      var positionRandom = Math.floor(Math.random() * charsetLength);
-      newPassword += selectedCharset.charAt(positionRandom);
-    }
-    return newPassword;
+  // Set the password length - exit if not successful
+  if (getUserInput(promptPasswordLengthText, pwdLengthSelected, updatePasswordLength) == false) return "";
+  // Set the password char types - exit if not successful
+  if (getUserInput(promptPasswordCharsText, pwdCharTypesSelected, updatePasswordChars) == false) return "";
+  //Generate and return password
+  var newPassword = "";
+  var selectedCharset = getSelectedPasswordCharset();
+  var charsetLength = selectedCharset.length;
+  for (let index = 0; index < pwdLengthSelected; index++) {
+    var positionRandom = Math.floor(Math.random() * charsetLength);
+    newPassword += selectedCharset.charAt(positionRandom);
   }
-  return "";
+  return newPassword;
 }
 
 // This function constructs the character set used for password generation, based on user's current selection 
 // Returns - character set for password generation as string
-function getPasswordCharset() {
+function getSelectedPasswordCharset() {
   var selectedCharset = "";
   for (var key in passwordCharset) {
     if (pwdCharTypesSelected.indexOf(key) != -1) selectedCharset += passwordCharset[key];
@@ -53,77 +56,74 @@ function getPasswordCharset() {
   return selectedCharset;
 }
 
-// This function prompts user to input password length, validates the same and returns true if successful 
-function setPasswordLength() {
+// This function prompts user to input a value, validates and saves the selection
+// Parameters - promptText - the text to be displayed when prompting the user to enter a value
+//              promptValue - the default value to be displayed when prompting the user 
+//              updateValueHandler - function that will validate and save the user entry
+// Returns - true if successful, false if operation cancelled by user
+function getUserInput(promptText, promptValue, updateValueHandler) {
   var isSelected = false;
-  var promptText = promptPasswordLengthText;
-  var promptValue = pwdLengthSelected;
-  // Loop prompts user until a correct selection has been made
+  var currentPromptText = promptText;
+  var currentPromptValue = promptValue;
+  // This Loop prompts the user for input until a correct selection has been made
   while (!isSelected) {
-    var usrSelection = prompt(promptText, promptValue);
+    var usrSelection = prompt(currentPromptText, currentPromptValue);
     if (usrSelection == undefined) {
       // User chose to cancel - so exit loop and return false
       break;
     }
     else {
-      var usrLength = parseInt(usrSelection);
-      if (!isNaN(usrLength) && usrLength >= 8 && usrLength <= 128) {
-        //Correct length selection - set the password length
-        pwdLengthSelected = usrLength;
-        isSelected = true;
-      }
+      // Update the user selection 
+      isSelected = updateValueHandler(usrSelection);
     }
     //For incorrect selection, update user prompt message accordingly
     if (!isSelected) {
-      promptText = promptIncorrectEntry + promptPasswordLengthText;
-      promptValue = usrSelection;
+      currentPromptText = promptIncorrectEntry + promptText;
+      currentPromptValue = usrSelection;
     }
   }
   return isSelected;
 }
 
-// This function prompts user to input password char types, validates the same and returns true if successful 
-function setPasswordChars() {
-  var isSelected = false;
-  var promptText = promptPasswordCharsText;
-  var promptValue = pwdCharTypesSelected;
-  // Loop prompts user until a correct selection has been made
-  while (!isSelected) {
-    var usrSelection = prompt(promptText, promptValue);
-    if (usrSelection == undefined) {
-      // User chose to cancel - so exit loop and return false
+// Validates and saves the length entered by the user 
+// Parameter - usrSelection - the length entered by the user as string
+// Returns - true if successful, false if the length is incorrect 
+function updatePasswordLength(usrSelection) {
+  var usrLength = parseInt(usrSelection);
+  if (!isNaN(usrLength) && usrLength >= 8 && usrLength <= 128) {
+    //Correct length selection - set the password length
+    pwdLengthSelected = usrLength;
+    return true;
+  }
+  return false;
+}
+
+// Validates and saves the char types selected by the user 
+// Parameter - usrSelection - the char types selected by the user as string
+// Returns - true if successful, false if the selection is incorrect 
+function updatePasswordChars(usrSelection) {
+  var usrCharTypes = "";
+  var arrUsrSelection = usrSelection.toLowerCase();
+  // Loop through user input characters
+  for (var i = 0; i < arrUsrSelection.length; i++) {
+    var element = arrUsrSelection[i];
+    if (passwordCharset[element] != undefined) {
+      // input character is valid, so add to char types, if not already included
+      if (usrCharTypes.indexOf(element) == -1) usrCharTypes += element;
+    } else if (element != " ") {
+      // input character is invalid, so reset char type array and break the for loop
+      usrCharTypes = "";
       break;
     }
-    else {
-      var usrCharTypes = "";
-      var arrUsrSelection = usrSelection.toLowerCase();
-      // Loop through user input characters
-      for (var i = 0; i < arrUsrSelection.length; i++) {
-        var element = arrUsrSelection[i];
-        if (passwordCharset[element] != undefined) {
-          // input character is valid, so add to char types, if not repeating
-          if (usrCharTypes.indexOf(element) == -1) usrCharTypes += element;
-        } else if (element != " ") {
-          // input character is invalid, so reset and break the for loop
-          usrCharTypes = "";
-          break;
-        }
-      }
-      if (usrCharTypes.length > 0) {
-        //Char types selected correctly - set the password char types
-        pwdCharTypesSelected = usrCharTypes;
-        isSelected = true;
-      }
-    }
-    //For incorrect selection, update user prompt message accordingly
-    if (!isSelected) {
-      promptText = promptIncorrectEntry + promptPasswordCharsText;
-      promptValue = usrSelection;
-    }
   }
-  return isSelected;
+  // Check if Char types selected correctly and set the password char types selection 
+  if (usrCharTypes.length > 0) {
+    pwdCharTypesSelected = usrCharTypes;
+    return true;
+  }
+  return false;
 }
 
-// Add event listener to generate button
+// Add event listener to generate button to output the password
 generateBtn.addEventListener("click", writePassword);
 
